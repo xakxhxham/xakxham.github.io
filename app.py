cba3265e7888b6a3587890e886bcb3592cf635f5
xakxhxham <chresthaxakxham@gmail.com>
2021-08-30 09:04:50 -0700

    test.0.01

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def Render():
+    return render_template("index.html")
+
+@app.route('/html_tutorial_home.html')
+def Render_index():
+    return render_template("html_tutorial_home.html")
+if __name__ == "__main__":
+    app.run(debug=True)
