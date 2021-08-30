af7bb7ab96b816dd8ca2831ea0704ca4618f153b
xakxhxham <chresthaxakxham@gmail.com>
2021-08-30 09:25:56 -0700

    Test.0.003

--- app.py
+++ app.py
@@ -8,5 +8,7 @@ def Render():
 @app.route('/html_tutorial_home.html')
 def Render_index():
     return render_template("html_tutorial_home.html")
+# if __name__ == "__main__":
+#     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0")
